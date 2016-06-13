dedf783d653944cf9c6e57e861389376b445d810
LSChyi <alan81920@gmail.com>
2016-06-14 01:23:33 +0800

    bind key data

--- configurator/app/app.module.js
+++ configurator/app/app.module.js
@@ -4,3 +4,12 @@ angular.module("ConfiguratorApp", [ "ngMaterial", "Key", "Layouts" ])
 			.primaryPalette('teal')
 			.accentPalette('blue');
 	})
+	.controller("appCtrl", function($scope) {
+		$scope.init_app = function() {
+			document.onkeydown = function(e) {
+				// broadcast keycode to every controller, and controller will do the map process to find the key
+				event.preventDefault();
+				console.log(e.code);
+			}
+		}
+	})
